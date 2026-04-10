0ac9ba27eb028b57b74c6419798becb608c35612
BettyDang <bettyx33_@hotmail.com>
2026-04-10 15:56:32 -0400

    chore : DB added

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const { connectDB } = require("./config/db");
+const app = require("./app");
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+            app.listen(5000, ()=>{
+            console.log("Server is running...");
+        });
+    } 
+    catch (error) 
+    {
+        console.log("Failed to start server", error);
+        process.exit(1);
+    }
+};
+
+startServer();
