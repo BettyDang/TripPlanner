d6e4501cc5a9fa9526fb3ebb5ef1a134ed3b14c7
BettyDang <bettyx33_@hotmail.com>
2026-05-03 20:20:09 -0400

    chore: route fixed

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'trips/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
